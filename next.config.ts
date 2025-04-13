f142f3ccdf9e7f269867c8e75b960e33ba054f0d
nickdieter52 <nickdieter52@gmail.com>
2025-04-13 18:50:43 +0000

    saving progress

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: 'https://wgjoajkwrtzafykrlnwo.supabase.co',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: 'pk_test_YmFsYW5jZWQtcXVldHphbC0xNS5jbGVyay5hY2NvdW50cy5kZXYk',
+    CLERK_SECRET_KEY: 'sk_test_QhkC5TLDPwgjOFs9FdF85f74rKHPrRjuwTPKHR5iSs',
+  },
 };
 
 export default nextConfig;
